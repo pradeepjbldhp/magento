ddb8f57266efb8a3bf6e0c79860fefaaa009deb1
pradeepjbldhp <rvnight@user-desktop.(none)>
2015-01-28 03:38:47 +0530

    magento new

---
+++ app/code/local/Pfay/Test/Block/Monblock.php
@@ -0,0 +1,8 @@
+<?php
+class Pfay_Test_Block_Monblock extends Mage_Core_Block_Template
+{
+     public function methodblock()
+     {
+         return 'informations about my block !!' ;
+     }
+}
---
+++ app/code/local/Pfay/Test/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+class Pfay_Test_IndexController extends Mage_Core_Controller_Front_Action
+{
+   public function indexAction ()
+   {
+     $this->loadLayout();
+     $this->renderLayout();
+   }
+  
+}
+?>
